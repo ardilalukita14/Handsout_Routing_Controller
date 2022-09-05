2603c6b7f27f42f666e409d515b3791ac7c37eac
ardilalukita14 <lukitasari623@gmail.com>
2022-09-05 19:05:13 +0700

    second commit

---
+++ 02_praktikum_web_lanjut_dua/app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return "NIM : 1941720043 <br> Nama : Ardila Lukita Sari"; 
+    }
+}
---
+++ 02_praktikum_web_lanjut_dua/app/Http/Controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+        return "Selamat Datang";
+    }
+
+    public function about() {
+        return "NIM : 1941720043 <br> Nama : Ardila Lukita Sari";
+    }
+
+    public function articles($id) {
+        return "Halaman artikel dengan ID $id";
+    }
+}
