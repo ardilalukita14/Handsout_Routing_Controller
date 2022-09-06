9ebce433ca68d5c593bb1cab791206adabffb554
ardilalukita14 <lukitasari623@gmail.com>
2022-09-06 18:56:47 +0700

    Menambahkan invokable route

--- 02_praktikum_web_lanjut_dua/routes/web.php
+++ 02_praktikum_web_lanjut_dua/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Blogs;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,3 +29,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'about']);
 Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+Route::get('/blogs', Blogs::class);
